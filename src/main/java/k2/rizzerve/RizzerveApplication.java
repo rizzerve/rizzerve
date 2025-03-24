41a2a4b5d7323855bef72570d2d7a9ffb0b4bf18
isaacboentoro <isaac.jesse@ui.ac.id>
2025-03-24 15:31:21 +0700

    initial commit

---
+++ src/main/java/k2/rizzerve/RizzerveApplication.java
@@ -0,0 +1,13 @@
+package k2.rizzerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzerveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k2/rizzerve/RizzerveApplicationTests.java
@@ -0,0 +1,13 @@
+package k2.rizzerve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizzerveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
