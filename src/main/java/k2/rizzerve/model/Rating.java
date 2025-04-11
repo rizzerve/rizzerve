6ff0f76a0d676ef12ef05e30171a247ccab6df1a
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-04-11 16:37:21 +0700

    [RED] implemented Rating model skeleton - will implement strategy design pattern

---
+++ src/main/java/k2/rizzerve/model/Rating.java
@@ -0,0 +1,14 @@
+package k2.rizzerve.model;
+
+import k2.rizzerve.strategy.RatingValidationStrategy;
+import lombok.Getter;
+
+@Getter
+public class Rating {
+
+
+    public Rating(String id, String menuId, String username, int ratingValue, RatingValidationStrategy strategy) {
+
+    }
+}
+
