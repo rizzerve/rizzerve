35191ee3f921258408a865a05adfd92c36eb508e
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-04-30 16:17:38 +0700

    Fixed controller and security config for authentication features

--- src/main/java/k2/rizzerve/config/SecurityConfig.java
+++ src/main/java/k2/rizzerve/config/SecurityConfig.java
@@ -2,10 +2,25 @@ package k2.rizzerve.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**", "/login", "/register").permitAll() // <-- tambahin "/register" di sini
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
