bad0f057cb4fafe35cb788d1b3caf4ffd07b55ad
isaacboentoro <isaac.jesse@ui.ac.id>
2025-05-01 21:25:32 +0700

    Open all endpoints temporarily

--- src/main/java/k2/rizzerve/config/SecurityConfig.java
+++ src/main/java/k2/rizzerve/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package k2.rizzerve.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,8 +14,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/login", "/register").permitAll() // <-- tambahin "/register" di sini
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll() // allow all endpoints
                 );
 
         return http.build();
