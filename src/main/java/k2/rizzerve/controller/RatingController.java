b68fdbc1c4a7f1bb14af833006583889979ccc8a
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-05-02 16:35:34 +0700

    [REFACTOR] Fixed controller

--- src/main/java/k2/rizzerve/controller/RatingController.java
+++ src/main/java/k2/rizzerve/controller/RatingController.java
@@ -1,16 +1,13 @@
 package k2.rizzerve.controller;
 
 import k2.rizzerve.command.CreateRatingCommand;
-import k2.rizzerve.command.DeleteRatingCommand;
-import k2.rizzerve.command.UpdateRatingCommand;
 import k2.rizzerve.model.Rating;
 import k2.rizzerve.repository.RatingRepository;
 import k2.rizzerve.service.RatingService;
-import k2.rizzerve.strategy.FiveStarRatingValidation;
+import k2.rizzerve.strategy.RatingValidationStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/ratings")
@@ -18,8 +15,24 @@ public class RatingController {
 
     private final RatingService ratingService;
     private final RatingRepository ratingRepository;
+    private final RatingValidationStrategy strategy;
 
     @Autowired
-    public RatingController(RatingService ratingService, RatingRepository ratingRepository) {
+    public RatingController(RatingService ratingService, RatingRepository ratingRepository, RatingValidationStrategy strategy) {
+        this.ratingService = ratingService;
+        this.ratingRepository = ratingRepository;
+        this.strategy = strategy;
+    }
 
-}}
+    @PostMapping
+    public ResponseEntity<Rating> createRating(
+            @RequestParam String id,
+            @RequestParam String menuId,
+            @RequestParam String username,
+            @RequestParam int ratingValue
+    ) {
+        CreateRatingCommand command = new CreateRatingCommand(id, menuId, username, ratingValue, strategy, ratingRepository);
+        Rating result = ratingService.executeCommand(command);
+        return ResponseEntity.ok(result);
+    }
+}
--- src/test/java/k2/rizzerve/controller/RatingControllerTest.java
+++ src/test/java/k2/rizzerve/controller/RatingControllerTest.java
@@ -2,8 +2,10 @@ package k2.rizzerve.controller;
 
 import k2.rizzerve.command.CreateRatingCommand;
 import k2.rizzerve.model.Rating;
+import k2.rizzerve.repository.RatingRepository;
 import k2.rizzerve.service.RatingService;
 import k2.rizzerve.strategy.FiveStarRatingValidation;
+import k2.rizzerve.strategy.RatingValidationStrategy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,23 +18,27 @@ import static org.mockito.Mockito.*;
 class RatingControllerTest {
 
     private RatingService ratingService;
+    private RatingRepository ratingRepository;
+    private RatingValidationStrategy strategy;
     private RatingController controller;
 
     @BeforeEach
     void setUp() {
         ratingService = mock(RatingService.class);
-        controller = new RatingController(ratingService);
+        ratingRepository = mock(RatingRepository.class);
+        strategy = new FiveStarRatingValidation();
+        controller = new RatingController(ratingService, ratingRepository, strategy);
     }
 
     @Test
     void testCreateRatingReturnsOk() {
-        Rating dummy = new Rating("r1", "M001", "usn", 5, new FiveStarRatingValidation());
+        Rating dummy = new Rating("r1", "M001", "usn", 5, strategy);
 
         when(ratingService.executeCommand(any(CreateRatingCommand.class))).thenReturn(dummy);
 
         ResponseEntity<Rating> response = controller.createRating("r1", "M001", "usn", 5);
 
-        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(200, response.getStatusCode().value());
         assertEquals(dummy, response.getBody());
     }
 }
