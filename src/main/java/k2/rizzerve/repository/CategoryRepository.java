8bf0cbb3bf0930b98ad8189bee45f8c819280bfb
agtyra <keziagtyra@gmail.com>
2025-05-09 19:18:14 +0700

    [GREEN] implement CategoryRepository

---
+++ src/main/java/k2/rizzerve/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package k2.rizzerve.repository;
+
+import k2.rizzerve.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> { }
