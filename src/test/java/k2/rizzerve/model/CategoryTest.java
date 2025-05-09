e30b02ba6c463b735f0c42837f083e7f50913622
agtyra <keziagtyra@gmail.com>
2025-05-09 18:36:35 +0700

    [RED] Add tests for Category Model

---
+++ src/test/java/k2/rizzerve/model/CategoryTest.java
@@ -0,0 +1,29 @@
+package k2.rizzerve.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void builderShouldCreateWithName() {
+        String expected = "Minuman";
+
+        Category c = new Category.Builder()
+                .name(expected)
+                .build();
+
+        assertNotNull(c);
+        assertNull(c.getId(), "ID should be null before persistence");
+        assertEquals(expected, c.getName());
+    }
+
+    @Test
+    void setterShouldModifyName() {
+        Category c = new Category();
+        assertNull(c.getName(), "Name should start out null");
+        c.setName("Makanan Ringan");
+        assertEquals("Makanan Ringan", c.getName());
+    }
+}
