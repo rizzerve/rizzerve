2a710996a5a6ee0107353d63d0cc923f17a0840d
agtyra <keziagtyra@gmail.com>
2025-05-09 18:40:42 +0700

    [GREEN] Implement Category Model

---
+++ src/main/java/k2/rizzerve/model/Category.java
@@ -0,0 +1,27 @@
+package k2.rizzerve.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Category(){}
+    private Category(Builder b) {
+        this.name = b.name;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public static class Builder {
+        private String name;
+        public Builder name(String n) { this.name = n; return this; }
+        public Category build()     { return new Category(this); }
+    }
+}
