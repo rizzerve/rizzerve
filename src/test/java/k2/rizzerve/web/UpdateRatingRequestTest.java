6927bba699b411c69aff4003a8630de8a99df3d3
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-05-09 19:28:02 +0700

    [RED] Add Tests for DTO implementation

---
+++ src/test/java/k2/rizzerve/web/UpdateRatingRequestTest.java
@@ -0,0 +1,30 @@
+package k2.rizzerve.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UpdateRatingRequestTest {
+
+    @Test
+    void publicFieldsAreWritableAndReadable() {
+        UpdateRatingRequest req = new UpdateRatingRequest();
+
+        req.menuId      = "MXYZ";
+        req.username    = "dan";
+        req.ratingValue = 3;
+
+        assertEquals("MXYZ",    req.menuId);
+        assertEquals("dan",     req.username);
+        assertEquals(3,         req.ratingValue);
+    }
+
+    @Test
+    void defaultsAreNullOrZero() {
+        UpdateRatingRequest req = new UpdateRatingRequest();
+        assertNull(req.menuId,       "menuId should default to null");
+        assertNull(req.username,     "username should default to null");
+        assertEquals(0, req.ratingValue,
+                "ratingValue should default to 0");
+    }
+}
