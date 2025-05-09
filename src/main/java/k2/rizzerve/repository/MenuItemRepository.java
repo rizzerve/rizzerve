ceb1b63cf745d4e03c0ca774b431bb12ba8a86e5
agtyra <keziagtyra@gmail.com>
2025-05-09 20:19:56 +0700

    [GREEN] Implement MenuItemRepository

---
+++ src/main/java/k2/rizzerve/repository/MenuItemRepository.java
@@ -0,0 +1,8 @@
+package k2.rizzerve.repository;
+
+import k2.rizzerve.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> { }
