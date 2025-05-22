afd60364ea71afecad28d7c5234d3ede6da9e0c6
agtyra <keziagtyra@gmail.com>
2025-05-22 16:35:20 +0700

    [GREEN] added Home and Public Menu Controller

---
+++ src/main/java/k2/rizzerve/controller/PublicMenuController.java
@@ -0,0 +1,34 @@
+package k2.rizzerve.controller;
+
+import k2.rizzerve.model.MenuItem;
+import k2.rizzerve.service.CategoryService;
+import k2.rizzerve.service.MenuItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
+public class PublicMenuController {
+
+    private final MenuItemService menuItemService;
+    private final CategoryService categoryService;
+
+    public PublicMenuController(MenuItemService menuItemService, CategoryService categoryService) {
+        this.menuItemService = menuItemService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/menu")
+    public String showPublicMenu(Model model) {
+        List<MenuItem> items = menuItemService.listAll();
+        Map<Long, List<MenuItem>> itemsByCategory = items.stream()
+                .collect(Collectors.groupingBy(item -> item.getCategory().getId()));
+        model.addAttribute("categories", categoryService.listAll());
+        model.addAttribute("itemsByCategory", itemsByCategory);
+        return "public_menu";
+    }
+}
